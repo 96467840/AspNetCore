8324751e5adcc6168b07337b14fc3141e64996ca
AndreyMurin <96467840@mail.ru>
2017-03-07 18:00:44 +0300

    adding gulp for sass, webpack for typescripts

---
+++ src/AspNetCore/gulpfile.js
@@ -0,0 +1,27 @@
+﻿/// <binding BeforeBuild='sass' ProjectOpened='default' />
+'use strict';
+
+var gulp = require( 'gulp' );
+var sass = require( 'gulp-sass' );
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+
+var project = require( './project.json' );
+
+var stylesrc = [project.webroot + '/sass/*.scss'];
+var stylesrc_watch = [project.webroot + '/sass/**/*.scss'];
+
+gulp.task('sass', function () {
+    return gulp.src(stylesrc)
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(project.webroot + '/css'));
+});
+
+gulp.task( 'watch', function () {
+    gulp.watch( stylesrc_watch, ['sass'] );
+} );
+
+gulp.task( 'default', ['watch', 'sass'] );
