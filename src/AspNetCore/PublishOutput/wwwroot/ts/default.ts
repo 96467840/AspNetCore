1a10834446070198cebc268e070d5ed3bd7a8f15
Andrey Murin <96467840@mail.ru>
2017-04-16 10:49:09 +0300

    публикация через гит

---
+++ src/AspNetCore/PublishOutput/wwwroot/ts/default.ts
@@ -0,0 +1,23 @@
+﻿import UTS = require('../lib/unobtrusive-typescript/dist/unobtrusive-typescript');
+//import UTS = require('unobtrusive-typescript');
+
+
+let _app = new UTS.App();
+_app
+    //.AddCollection(new UTS.ItemCollection(<UTS.BaseParams>{ Selector: '.jsc-uts' }, _app))
+    .AddDefaultCollections()
+    .Run({
+        before: function () { },
+        ready: function (app: UTS.App) {
+            UTS.Log('ready');
+            app.FindCollectionBySelector('.jsc-uts').Invoke('hello');
+
+            app.FindCollectionBySelector('.jsc-uts').Invoke('Say', null, ['Hello world', false]);
+
+            /*let d = new UTS.Dialog(<UTS.DialogParams>{
+                Show: true, Title: 'Test', Body: $('<p>Test dialog</p>'),
+                Buttons: [new UTS.Button('Ok', function () { UTS.Log('Ok click'); }, 'btn btn-default js-close', 'button', { type: 'superbutton', 'data-s':'hello' })]
+            }, 0, 'Close');/**/
+
+        }
+    });
