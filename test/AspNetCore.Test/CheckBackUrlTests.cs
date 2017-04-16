5f261611487150944828f1385ae5b0c5ee0a9888
Andrey Murin <96467840@mail.ru>
2017-04-16 10:02:43 +0300

    добавлен проект с тестами

---
+++ test/AspNetCore.Test/CheckBackUrlTests.cs
@@ -0,0 +1,21 @@
+using AspNetCoreComponentLibrary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace AspNetCore.Test
+{
+    public class CheckBackUrlTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //var controller = new Controller2Garin(null, null);
+            //controller.ControllerContext = new ControllerContext();
+            //controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            Assert.True(true);
+        }
+    }
+}
