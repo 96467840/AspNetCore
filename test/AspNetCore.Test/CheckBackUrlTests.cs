2dbdfb5882602127faeb899a7db0088a9b7287da
Andrey Murin <96467840@mail.ru>
2017-04-22 10:47:19 +0300

    исправил логическую ошибку в тестах,  и расширил тест

--- test/AspNetCore.Test/CheckBackUrlTests.cs
+++ test/AspNetCore.Test/CheckBackUrlTests.cs
@@ -16,9 +16,18 @@ namespace AspNetCore.Test
         }
 
         [Theory]
-        [InlineData("localhost")]
-        [InlineData("2garin.com")]
-        [InlineData("www.2garin.com")]
+        [InlineData("http://localhost")]
+        [InlineData("https://localhost/?ewrw=wer")]
+        [InlineData("http://2garin.com")]
+        [InlineData("//2garin.com")]
+        [InlineData("http://www.2garin.com")]
+        [InlineData("http://www.2garin.com/")]
+        [InlineData("http://www.2garin.com/path/")]
+        [InlineData("https://www.2garin.com/?param1=wsfhw")]
+        [InlineData("https://www.2garin.com:8080/?param1=wsfhw")]
+        [InlineData("//www.2garin.com")]
+        [InlineData("relative/path")]
+        [InlineData("/absolute/path")]
         public void TestValidHosts(string host)
         {
             //var controller = new Controller2Garin(null, null);
@@ -26,6 +35,20 @@ namespace AspNetCore.Test
             //controller.ControllerContext.HttpContext = new DefaultHttpContext();
 
             Assert.True(Utils.CheckBackUrl(Sites, host));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("http://ewrj")]
+        [InlineData("http://localhost1")]
+        [InlineData("http://2garin.ru")]
+        [InlineData("http://2garin.ru/2garin.com")]
+        [InlineData("//2garin.ru")]
+        [InlineData("http://2garin.com.2garin.ru")]
+        public void TestNotValidHosts(string host)
+        {
+            Assert.False(Utils.CheckBackUrl(Sites, host));
         }
     }
 }
